147957ed47a008b609f0b06606c29a70e3be0c1d
adamk06pl <01197821@pw.edu.pl>
2024-12-19 15:47:04 +0100

    Implementacja metody podstawienia wstecznego w backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,22 +1,30 @@
-#include "backsubst.h"
-/**
- * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
- * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
- * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
- */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+#include "gauss.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+/* Funkcja wykonuje podstawianie wsteczne */
+/* Zakłada, że macierz "mat" jest górnotrójkątna */
+/* Rozwiązuje układ równań Ax = b */
+int backsubst(Matrix *x, Matrix *a, Matrix *b) {
+    int rows = a->r; // Liczba wierszy macierzy
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
 
-				return 0;
+    // Algorytm podstawiania wstecznego
+    for (int i = rows - 1; i >= 0; --i) {
+        double sum = 0.0;
+
+        for (int j = i + 1; j < a->c; ++j) {
+            sum += a->data[i][j] * x->data[j][0];
+        }
+
+        if (a->data[i][i] == 0.0) {
+            fprintf(stderr, "Macierz osobliwa - dzielenie przez zero.\n");
+            return 1;
+        }
+
+        x->data[i][0] = (b->data[i][0] - sum) / a->data[i][i];
+    }
+
+    return 0; // Sukces
 }
-
 
